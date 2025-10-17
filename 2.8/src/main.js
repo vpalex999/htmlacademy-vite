76f3519fafdf3ca3d41f5b01eb792feccdfdb1fd
vpalex999 <vpalex@mail.ru>
2025-10-17 20:30:20 +0500

    2.8:stage-1

---
+++ 2.8/src/main.js
@@ -0,0 +1,11 @@
+import './style.css';
+import viteLogo from '/vite.svg';
+
+document.querySelector ('#app').innerHTML = `
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <h1>Hello Vite!</h1>
+  </div>
+`;
