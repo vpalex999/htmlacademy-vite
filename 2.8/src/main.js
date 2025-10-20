9ed0a679c6d358a3b063cedbed1b39e3c28528dd
vpalex999 <vpalex@mail.ru>
2025-10-20 20:37:56 +0500

    2.8:stage-4

--- 2.8/src/main.js
+++ 2.8/src/main.js
@@ -5,6 +5,7 @@ document.querySelector ('#app').innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
+      <svg class="svg_sprite"><use href="#vite"></svg>
     </a>
     <a href="/pages/about/">О нас</a>
     <a href="/pages/news/">новости</a>
--- 2.8/vite.config.js
+++ 2.8/vite.config.js
@@ -1,13 +1,28 @@
 import {defineConfig} from 'vite';
 import {resolve} from 'path';
+import {ViteImageOptimizer} from 'vite-plugin-image-optimizer';
+import createSvgSpritePlugin from 'vite-plugin-svg-spriter';
 
 export default defineConfig ({
+  plugins: [
+    createSvgSpritePlugin ({
+      svgFolder: './assets/images/svg/',
+    }),
+    ViteImageOptimizer ({
+      jpeg: {
+        quality: 75,
+      },
+      png: {
+        quality: 75,
+      },
+    }),
+  ],
   build: {
     rollupOptions: {
       input: {
         index: resolve (__dirname, 'index.html'),
         about: resolve (__dirname, 'pages/about/index.html'),
-        news: resolve(__dirname, 'pages/news/index.html')
+        news: resolve (__dirname, 'pages/news/index.html'),
       },
     },
   },
