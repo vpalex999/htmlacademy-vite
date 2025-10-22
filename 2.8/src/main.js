962d3071d06fd3d96efbfdc0ea56b58639e0197a
vpalex999 <vpalex@mail.ru>
2025-10-22 18:59:11 +0500

    2.8:stage-5

--- 2.8/src/main.js
+++ 2.8/src/main.js
@@ -5,7 +5,7 @@ document.querySelector ('#app').innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
-      <svg class="svg_sprite"><use href="#vite"></svg>
+      <svg class="svg_sprite"><use href="#vite"></use></svg>
     </a>
     <a href="/pages/about/">О нас</a>
     <a href="/pages/news/">новости</a>
