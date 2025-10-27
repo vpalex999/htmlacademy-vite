e5ca9fef6d11eaf1c26701e94df3db30ff8284cb
vpalex999 <vpalex@mail.ru>
2025-10-27 21:37:38 +0500

    2.8:stage-7

--- 2.8/src/main.js
+++ 2.8/src/main.js
@@ -1,11 +1,9 @@
 import './style.scss';
-import viteLogo from '/vite.svg';
 
 document.querySelector ('#app').innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-      <svg class="svg_sprite"><use href="#vite"></use></svg>
+      <svg class="svg_sprite logo"><use href="#vite"></use></svg>
     </a>
     <a href="/pages/about/">О нас</a>
     <a href="/pages/news/">новости</a>
--- 2.8/vite.config.js
+++ 2.8/vite.config.js
@@ -3,13 +3,19 @@ import {resolve} from 'path';
 import {ViteImageOptimizer} from 'vite-plugin-image-optimizer';
 import createSvgSpritePlugin from 'vite-plugin-svg-spriter';
 
+const FRONT_PATH = 'src';
+
 export default defineConfig ({
+  root: 'src',
   plugins: [
     createSvgSpritePlugin ({
-      svgFolder: './assets/images/svg/',
+      svgFolder: resolve (__dirname, FRONT_PATH, 'assets/images/svg/'),
     }),
     ViteImageOptimizer ({
       jpeg: {
+        quality: 75,
+      },
+      jpg: {
         quality: 75,
       },
       png: {
@@ -22,9 +28,9 @@ export default defineConfig ({
     sourcemap: true,
     rollupOptions: {
       input: {
-        index: resolve (__dirname, 'index.html'),
-        about: resolve (__dirname, 'pages/about/index.html'),
-        news: resolve (__dirname, 'pages/news/index.html'),
+        index: resolve (__dirname, FRONT_PATH, 'index.html'),
+        about: resolve (__dirname, FRONT_PATH, 'pages/about/index.html'),
+        news: resolve (__dirname, FRONT_PATH, 'pages/news/index.html'),
       },
     },
   },
